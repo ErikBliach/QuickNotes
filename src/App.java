576572296bba346e1b85557098e05973b1b52592
ErikBliach <erikbliacher777@gmail.com>
2024-06-06 11:38:16 -0400

    Added button to save notes and button to view saved notes (currently don't do anything), changed how layout of frame to open notes is coded

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
 import java.awt.event.*;
+import javax.swing.*;
 
 
 public class App {
@@ -11,20 +8,26 @@ public class App {
 
         //Initializing aspects of starting window
         JFrame quickNotes = new JFrame("QuickNotes");
-        JLabel label = new JLabel("Open a new note with the button.", SwingConstants.CENTER);
+        JLabel label = new JLabel("<html>Welcome to QuickNotes!<p>Open a new note with the Note button.<p>Check your saved notes with the Saved notes button.</html>", SwingConstants.CENTER);//<html> and <p> allow the text to be shown on multiple lines
         JButton noteButton = new JButton("Note");
+        JButton savedNotes = new JButton("Saved notes");
         JPanel panel = new JPanel();
 
         //configurations for frame of window to open a note
         quickNotes.setResizable(false);
         quickNotes.add(panel);
-        quickNotes.setSize(300, 150);
+        quickNotes.setSize(350, 150);
         quickNotes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         quickNotes.setVisible(true);
 
         //configurations for panel within frame, panel contains text and button
-        panel.add(noteButton);
+        panel.setLayout(new BorderLayout());
         panel.add(label);
+        panel.add(savedNotes, BorderLayout.SOUTH);
+        panel.add(noteButton, BorderLayout.SOUTH);
+        
+
+        //panel.add(noteButton);
         panel.setSize(300,150);
 
         //to open a new note whenever the button is pressed
--- src/quickNote.java
+++ src/quickNote.java
@@ -1,21 +1,33 @@
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class quickNote {
     public void newNote(){
 
     //initializing aspects of notes
     JFrame note = new JFrame("quickNote");
-    JTextArea textArea = new JTextArea();
-    
+    JTextArea textArea = new JTextArea(8,8);
+    JPanel thePanel = new JPanel();
+
     //configurations for frame of notes
+    note.add(thePanel);
     note.setResizable(false);
-    note.add(textArea);
-    note.setSize(200, 200);
+    note.setSize(200, 195);
     note.setVisible(true);
+
+    //configurations for panel that holds area for text and save note button
+    thePanel.setLayout(new BorderLayout());
+    thePanel.add(new JButton("Save"), BorderLayout.SOUTH);
+    thePanel.add(textArea, BorderLayout.NORTH);
 
     //prevents text from going off the screen horizontally, or wrapping in weird ways
     textArea.setLineWrap(true);
     textArea.setWrapStyleWord(true);
+    textArea.setBounds(0, 0, 180, 200);
     }
 }
+
+
