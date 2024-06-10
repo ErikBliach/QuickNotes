64129693447de9e6c1f5d3ff5b4e5fbf83de831e
ErikBliach <erikbliacher777@gmail.com>
2024-06-10 11:28:59 -0400

    Added color to notes

--- src/quickNote.java
+++ src/quickNote.java
@@ -1,8 +1,6 @@
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
 import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.*;
 
 public class quickNote {
     public void newNote(){
@@ -11,20 +9,27 @@ public class quickNote {
     JFrame note = new JFrame("quickNote");
     JTextArea textArea = new JTextArea(8,8);
     JPanel thePanel = new JPanel(new BorderLayout());
+    JButton save = new JButton("Save");
+    Color noteColor = new Color(245, 245, 149);
+    Color buttonColor = new Color(239, 235, 106);
 
     //configurations for frame of notes
     note.add(thePanel);
     note.setResizable(false);
-    note.setSize(200, 195);
+    note.setSize(200, 193);
     note.setVisible(true);
 
     //configurations for panel that holds area for text and save note button
-    thePanel.add(new JButton("Save"), BorderLayout.SOUTH);
+    thePanel.add(save, BorderLayout.SOUTH);
     thePanel.add(textArea, BorderLayout.NORTH);
+
+    save.setBackground(buttonColor); //sets color of button
 
     textArea.setLineWrap(true); //prevents text from going off screen horizontally
     textArea.setWrapStyleWord(true); //prevents words from being cut off
     textArea.setBounds(0, 0, 180, 200); //size of text area
+    textArea.setPreferredSize(textArea.getPreferredSize()); //stops text area from expanding vertically
+    textArea.setBackground(noteColor); //sets color of text area
     }
 }
 
